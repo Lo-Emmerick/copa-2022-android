fa5ae5e45e7277a419d823d1cf7ea5d2a989f9b5
Lo-Emmerick <lorrainemmerick@gmail.com>
2025-12-28 03:29:47 -0300

    Criação da tarefa de notificação

--- app/src/main/kotlin/me/dio/copa/catar/features/MainScreen.kt
+++ app/src/main/kotlin/me/dio/copa/catar/features/MainScreen.kt
@@ -1,6 +1,7 @@
 package me.dio.copa.catar.features
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -31,21 +32,23 @@ import me.dio.copa.catar.domain.model.MatchDomain
 import me.dio.copa.catar.domain.model.TeamDomain
 import me.dio.copa.catar.ui.theme.Shapes
 
+typealias NotificationOnClick = (match: MatchDomain) -> Unit
+
 @Composable
-fun MainScreen(matches: List<MatchDomain>) {
+fun MainScreen(matches: List<MatchDomain>, onNotificationClick: NotificationOnClick) {
     Box(modifier = Modifier
         .fillMaxSize()
         .padding(8.dp)) {
         LazyColumn (verticalArrangement = Arrangement.spacedBy(8.dp)) {
             items(matches){ match ->
-                MatchInfo(match)
+                MatchInfo(match, onNotificationClick)
             }
         }
     }
 }
 
 @Composable
-fun MatchInfo(match: MatchDomain) {
+fun MatchInfo(match: MatchDomain, onNotificationClick: NotificationOnClick) {
     Card (
         shape = Shapes.large,
         modifier = Modifier.fillMaxWidth()
@@ -58,7 +61,7 @@ fun MatchInfo(match: MatchDomain) {
                 modifier = Modifier.height(160.dp)
             )
             Column(modifier = Modifier.padding(16.dp)) {
-                Notification(match)
+                Notification(match, onNotificationClick)
                 Title(match)
                 Teams(match)
             }
@@ -67,7 +70,7 @@ fun MatchInfo(match: MatchDomain) {
 }
 
 @Composable
-fun Notification(match: MatchDomain) {
+fun Notification(match: MatchDomain, onClick: NotificationOnClick) {
     Row(
         modifier = Modifier.fillMaxWidth(),
         horizontalArrangement = Arrangement.End
@@ -77,6 +80,9 @@ fun Notification(match: MatchDomain) {
 
         Image(
             painter = painterResource(id = drawable),
+            modifier = Modifier.clickable { 
+                onClick(match)
+            },
             contentDescription = null
         )
     }
